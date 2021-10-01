a45f391ac5548142ebbdba669ce90bed12039288
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-01 11:09:26 -0300

    :sparkles: Criando context dos temas

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, ReactNode, useState } from 'react'
+import dark from '../styles/themes/dark'
+import light from '../styles/themes/light'
+
+type Theme = {
+  title: string;
+
+  colors: {
+    background: string;
+  }
+}
+
+type themeContextType = {
+  theme: Theme;
+  toggleTheme() : void;
+}
+
+export const themeContext = createContext({} as themeContextType)
+
+type ThemeContextProviderProps = {
+  children: ReactNode
+}
+
+export function ThemeContextProvider (props: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState<Theme>(light)
+
+  function toggleTheme () {
+    return setTheme(theme.title === 'light' ? dark : light)
+  }
+
+  return (
+    <themeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </themeContext.Provider>
+  )
+}
