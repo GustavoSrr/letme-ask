dbb6d51baaea17494cce2b065bee718785d91aee
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-01 11:03:26 -0300

    :truck: Transformando o Header em um componente

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  padding: 24px;
+  border-bottom: 1px solid var(--borderGrayColor);
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > img {
+    max-height: 45px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2px;
+
+    button {
+      padding: 16px;
+    }
+  }
+`
