d32c526925eaf1afc166eaa1279172af9fa410e1
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-12 17:28:46 -0300

    ✨ Adicionando toasts e botão para mudar o tema

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useAuth } from '../../hooks/useAuth'
 import { Link, useHistory } from 'react-router-dom'
 
@@ -10,9 +10,14 @@ import { database } from '../../services/firebase'
 import Illustration from '../../assets/images/illustration.svg'
 
 import { Container, Aside, Content, Main, Form } from './styles'
+import { useTheme } from '../../hooks/useTheme'
+import { Toaster } from 'react-hot-toast'
+import { ThemeContext } from 'styled-components'
 
 export const NewRoom: React.FC = () => {
   const { user } = useAuth()
+  const { toggleTheme } = useTheme()
+  const { colors } = useContext(ThemeContext)
   const history = useHistory()
   const [newRoom, setNewRoom] = React.useState('')
 
@@ -31,12 +36,51 @@ export const NewRoom: React.FC = () => {
 
   return (
     <Container>
+      <Toaster
+        toastOptions={{
+          success: {
+            style: {
+              borderRadius: '8px',
+              backgroundColor: colors.textAreaColor,
+              color: colors.textTitleColor,
+              boxShadow: 'none'
+            },
+            iconTheme: {
+              primary: '#2fd373',
+              secondary: colors.textAreaColor
+            }
+          },
+          error: {
+            style: {
+              borderRadius: '8px',
+              backgroundColor: colors.textAreaColor,
+              color: colors.textTitleColor,
+              boxShadow: 'none'
+            },
+            iconTheme: {
+              primary: '#e73f5d',
+              secondary: colors.textAreaColor
+            }
+          }
+        }}
+      />
       <Aside>
         <img src={Illustration} alt="Ilustração da home" />
         <strong>Toda pergunta tem uma resposta.</strong>
         <p>Aprenda e compartilhe conhecimento com outras pessoas.</p>
       </Aside>
       <Main>
+        <Button
+          title="Alterar tema"
+          id="HomeThemeButton"
+          rounded
+          transparent
+          onClick={() => toggleTheme()}
+        >
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 0.75C5.89399 0.75 1.75 4.88764 1.75 10C1.75 15.106 5.88764 19.25 11 19.25C13.5909 19.25 15.9773 18.1803 17.6876 16.3898C12.6795 16.8925 8.2591 12.9542 8.2591 7.84781C8.2591 5.01724 9.64705 2.3949 11.9452 0.798134C11.6313 0.766129 11.3159 0.750047 11 0.75ZM11 0.75V0V0.75ZM12.6027 0.383822C12.6028 0.383784 12.6028 0.383745 12.6029 0.383707L12.6027 0.383822Z" stroke="black" strokeWidth="1.3" />
+          </svg>
+        </Button>
         <Content>
           <Logo large/>
           <h2>Criar uma nova sala</h2>
