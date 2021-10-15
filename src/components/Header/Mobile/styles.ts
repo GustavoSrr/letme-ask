f03fb2f796a0850010d372829bb4175c679a3f8f
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-14 21:26:54 -0300

    📱 Copiando arquivos do header atual para pasta Header/Mobile

---
+++ src/components/Header/Mobile/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  position: sticky;
+  top: 0;
+  background-color: var(--backgroundColor);
+  padding: 24px;
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > img {
+    max-height: 45px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2px;
+
+    button {
+      padding: 16px;
+    }
+  }
+`
